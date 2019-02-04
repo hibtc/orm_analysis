92aef1173cfe52a9b08a13875c8c66b72fd4999c
Thomas Gläßle <t_glaessle@gmx.de>
2019-02-04 13:58:35 +0100

    Add docstring and improve variable name

--- quadscan.py
+++ quadscan.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 
 
 def mean_values(data):
+    """
+    Return NxMx2 array of averaged monitor measurements for the
+    N optics, M monitors and 2 spatial dimensions x/y.
+    """
     monitors = data['monitors']
     return np.array([
         np.mean([
@@ -19,12 +23,16 @@ def mean_values(data):
             for record in group
             for readout in [record['readout']]
         ][2:], axis=0)
-        for value, group in itertools.groupby(
+        for optics, group in itertools.groupby(
                 data['records'], key=lambda r: r['optics'])
     ])
 
 
 def stddevs(data):
+    """
+    Return NxMx2 array of standard deviations of monitor measurements
+    for the N optics, M monitors and 2 spatial dimensions x/y.
+    """
     monitors = data['monitors']
     return np.array([
         np.var([
@@ -35,13 +43,14 @@ def stddevs(data):
             ]
             for record in group
             for readout in [record['readout']]
-        ][1:], axis=0, ddof=1) ** 0.5
-        for value, group in itertools.groupby(
+        ][0:], axis=0, ddof=0) ** 0.5
+        for optics, group in itertools.groupby(
                 data['records'], key=lambda r: r['optics'])
     ])
 
 
 def read_file(filename):
+    """Read a text file, return as string."""
     with open(filename) as f:
         return f.read()
 
@@ -56,17 +65,17 @@ def main():
     means = [mean_values(data) for data in raw_data]
     errors = [stddevs(data) for data in raw_data]
 
-    values = [
+    kl_values = [
         np.array([
-            sum(value.values())
-            for value, group in itertools.groupby(
+            sum(optics.values())
+            for optics, group in itertools.groupby(
                     data['records'], key=lambda r: r['optics'])
         ])
         for data in raw_data
     ]
 
-    for vals, mean, err in zip(values, means, errors):
-        plt.errorbar(vals, mean[:, 0, 0], err[:, 0, 0])
+    for kl, mean, err in zip(kl_values, means, errors):
+        plt.errorbar(kl, mean[:, 0, 0], err[:, 0, 0])
         plt.show()
 
 
