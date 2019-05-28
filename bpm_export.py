8225b6bedb90c4fdc89fd6cb8e27041af6c98442
Thomas Gläßle <t_glaessle@gmx.de>
2019-05-28 18:23:41 +0200

    Simplify, export only positions not widths
    The formulas for errors/widths was probably wrong anyway.

--- bpm_export.py
+++ bpm_export.py
@@ -5,55 +5,21 @@ error using our own function, and then print out all entries in a simple TXT
 format.
 
 Usage:
-    ./bpm_export.py [-m METHOD] <CSV_FILES>...
-
-Options:
-    -m METHOD, --mean METHOD        Set method to calculate mean value and
-                                    standard error. Possible values are
-                                    {full, fwhm} [default: weighted]
+    ./bpm_export.py <CSV_FILES>...
 """
 
 from docopt import docopt
 
 import pandas as pd
 from time import strptime, mktime
-from math import sqrt, log
-
-
-# conversion factor from FWHM to standard deviation for normal distributions:
-fwhm_to_stddev = 2 * sqrt(2 * log(2))
-
-
-def builtin_mean_func(data, custom):
-    return {
-        'x': custom['Schwerpunkt X'],
-        'y': custom['Schwerpunkt Y'],
-        'sigx': custom['FWHM X'] * fwhm_to_stddev,
-        'sigy': custom['FWHM Y'] * fwhm_to_stddev,
-        'errx': 0,
-        'erry': 0,
-    }
-
-
-def weighted_mean_func(data, custom):
-    x, sigx, errx = weighted_mean(data['Drahtposition x'], data['X'])
-    y, sigy, erry = weighted_mean(data['Drahtposition y'], data['Y'])
-    return {'x': x, 'sigx': sigx, 'errx': errx,
-            'y': y, 'sigy': sigy, 'erry': erry}
 
 
 def weighted_mean(x, w):
-    """Calculate weighted mean and standard error according to
-    https://en.wikipedia.org/wiki/Weighted_arithmetic_mean#Bootstrapping_validation
-    """
-    w_tot = sum(w)
-    x_mean = sum(x * w) / w_tot
-    stddev = sum(w * (x - x_mean)**2)**0.5 / w_tot
-    sig_sq = sum(w**2 * (x - x_mean)**2) / w_tot * len(x) / (len(x) - 1)
-    return x_mean, stddev, sig_sq**0.5
+    """Calculate weighted mean of a vector x with weights w."""
+    return sum(x * w) / sum(w)
 
 
-def csv_to_tab(fnames, mean_func=weighted_mean):
+def csv_to_tab(fnames):
     """Read a list of CSV files, calculate mean and standard error from the
     beam profiles using the given function, and return as pandas DataFrame."""
     dicts = []
@@ -62,12 +28,19 @@ def csv_to_tab(fnames, mean_func=weighted_mean):
         time = mktime(strptime(header['Startzeit'], '%d.%m.%Y %H:%M:%S'))
         dicts.append({
             'bpm': header['Gerät'],
+            'x_full': weighted_mean(data['Drahtposition x'], data['X']),
+            'y_full': weighted_mean(data['Drahtposition y'], data['Y']),
+            'x_fwhm': custom['Schwerpunkt X'],
+            'y_fwhm': custom['Schwerpunkt Y'],
             'cycle': header['Zyklus ID'],
             'time': round(time),
-            **mean_func(data, custom)
         })
-    return pd.DataFrame(
-        dicts, columns=['bpm', 'x', 'y', 'sigx', 'sigy', 'cycle', 'time'])
+    return pd.DataFrame(dicts, columns=[
+        'bpm',
+        'x_full', 'y_full',
+        'x_fwhm', 'y_fwhm',
+        'cycle', 'time',
+    ])
 
 
 def parse_csv_export(fname):
@@ -93,17 +66,9 @@ def _parse_section(lines, start, end):
         yield line.strip().split(';')
 
 
-MEAN_FUNCS = {
-    'full': weighted_mean_func,
-    'fwhm': builtin_mean_func,
-}
-
-
 def main(args=None):
     opts = docopt(__doc__, args)
-    df = csv_to_tab(
-        opts['<CSV_FILES>'],
-        MEAN_FUNCS[opts['--method']])
+    df = csv_to_tab(opts['<CSV_FILES>'])
     print(df.to_string())
 
 
