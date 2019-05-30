8715bf566f924ba2315ef4a8a2e7f103e1a63cb7
Thomas Gläßle <t_glaessle@gmx.de>
2019-05-30 10:21:57 +0200

    Sanitize some command line options for orm_info

--- orm_info.py
+++ orm_info.py
@@ -3,11 +3,15 @@
 orm_show
 
 Usage:
-    ./orm_info.py [-e ERRORS]... [-o DIR] [MODEL] [RECORDS]...
+    ./orm_info.py [-o DIR] [-m MODEL] [-b] [--base-orbit]
+                  [-e ERRORS]... [RECORDS]...
 
 Options:
     -e FILE, --errors FILE      apply errors from file
     -o DIR, --output DIR        set output directory [default: results]
+    -m PATH, --model PATH       model path [default: ../hit_models/hht3]
+    -b, --backtrack             use backtracking
+    --base-orbit                plot unmodified orbit as well
 
 Arguments:
     MODEL                       madx model file
@@ -27,32 +31,35 @@ from orm_util import Analysis
 def main(args=None):
     opts = docopt(__doc__, args)
 
-    model = opts['MODEL'] or 'hit_models/hht3'
+    prefix = opts['--output'] + '/'
+    os.makedirs(prefix, exist_ok=True)
+
+    model = opts['--model']
     record_files = (
         opts['RECORDS'] or
         'data/2018-10-20-orm_measurements/M8-E108-F1-I9-G1/*.yml')
 
     ana = Analysis.app(model, record_files)
-    ana.init()
-    #ana.backtrack(['h1dg1g', 'h1dg2g', 'h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g'])
-    base_orm = ana.model_orbits
+
+    if opts['--base-orbit']:
+        ana.init()
+        base_orm = ana.model_orbits
+    else:
+        base_orm = None
 
     for filename in opts['--errors']:
         import_errors(ana.model, yaml.load_file(filename))
+
     ana.init({})
-    ana.backtrack(['h1dg1g', 'h1dg2g', 'h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g'])
-    #ana.backtrack(['h3dg3g', 'b3dg2g', 'b3dg3g'])
 
-    #print(ana.backtrack(['h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g']))
-
-    prefix = opts['--output'] + '/'
-    os.makedirs(prefix, exist_ok=True)
+    if opts['--backtrack']:
+        ana._init_twiss = ana.extrapolate(
+            ['t3dg2g', 't3dg1g', 't3df1'], to='#s')
 
     ana.info()
-    ana.plot_monitors(['g3dg5g'], base_orm=base_orm)
-    #ana.plot_steerers(['h1ms4v'], base_orm=base_orm)
-    ana.plot_orbit()
-    #ana.plot_monitors(['g3dg3g'], save_to=prefix, base_orm=base_orm)
+    ana.plot_monitors(base_orm=base_orm, save_to=f'{prefix}mon_response')
+    ana.plot_steerers(base_orm=base_orm, save_to=f'{prefix}mon_response')
+    ana.plot_orbit(save_to=f'{prefix}orbit')
 
 
 if __name__ == '__main__':
