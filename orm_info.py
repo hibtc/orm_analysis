42b6b502927a97fe143ddc28c982a57d7e6d0d67
Thomas Gläßle <t_glaessle@gmx.de>
2019-05-30 10:13:54 +0200

    Wrap orm_info main script in a function

--- orm_info.py
+++ orm_info.py
@@ -23,32 +23,38 @@ from madgui.model.errors import import_errors
 
 from orm_util import Analysis
 
-opts = docopt(__doc__)
 
-model = opts['MODEL'] or 'hit_models/hht3'
-record_files = (
-    opts['RECORDS'] or
-    'data/2018-10-20-orm_measurements/M8-E108-F1-I9-G1/*.yml')
+def main(args=None):
+    opts = docopt(__doc__, args)
+
+    model = opts['MODEL'] or 'hit_models/hht3'
+    record_files = (
+        opts['RECORDS'] or
+        'data/2018-10-20-orm_measurements/M8-E108-F1-I9-G1/*.yml')
+
+    ana = Analysis.app(model, record_files)
+    ana.init()
+    #ana.backtrack(['h1dg1g', 'h1dg2g', 'h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g'])
+    base_orm = ana.model_orbits
+
+    for filename in opts['--errors']:
+        import_errors(ana.model, yaml.load_file(filename))
+    ana.init({})
+    ana.backtrack(['h1dg1g', 'h1dg2g', 'h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g'])
+    #ana.backtrack(['h3dg3g', 'b3dg2g', 'b3dg3g'])
+
+    #print(ana.backtrack(['h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g']))
+
+    prefix = opts['--output'] + '/'
+    os.makedirs(prefix, exist_ok=True)
+
+    ana.info()
+    ana.plot_monitors(['g3dg5g'], base_orm=base_orm)
+    #ana.plot_steerers(['h1ms4v'], base_orm=base_orm)
+    ana.plot_orbit()
+    #ana.plot_monitors(['g3dg3g'], save_to=prefix, base_orm=base_orm)
 
 
-ana = Analysis.app(model, record_files)
-ana.init()
-#ana.backtrack(['h1dg1g', 'h1dg2g', 'h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g'])
-base_orm = ana.model_orbits
-
-for filename in opts['--errors']:
-    import_errors(ana.model, yaml.load_file(filename))
-ana.init({})
-ana.backtrack(['h1dg1g', 'h1dg2g', 'h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g'])
-#ana.backtrack(['h3dg3g', 'b3dg2g', 'b3dg3g'])
-
-#print(ana.backtrack(['h2dg2g', 'h3dg3g', 'b3dg2g', 'b3dg3g']))
-
-prefix = opts['--output'] + '/'
-os.makedirs(prefix, exist_ok=True)
-
-ana.info()
-ana.plot_monitors(['g3dg5g'], base_orm=base_orm)
-#ana.plot_steerers(['h1ms4v'], base_orm=base_orm)
-ana.plot_orbit()
-#ana.plot_monitors(['g3dg3g'], save_to=prefix, base_orm=base_orm)
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[1:]))
