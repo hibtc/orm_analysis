8bc33d16e2850981a41270d74485826d78248d48
Thomas Gläßle <t_glaessle@gmx.de>
2019-06-07 15:56:06 +0200

    Commit some changes in the plots

--- orm_plot.py
+++ orm_plot.py
@@ -3,6 +3,7 @@ Plot utility functions for showing results of the ORM analysis.
 """
 
 import matplotlib.pyplot as plt
+from madgui.plot.twissfigure import plot_element_indicators, with_outline
 
 
 def make_orbit_plots(
@@ -10,14 +11,15 @@ def make_orbit_plots(
         save_to=None, base_orbit=None):
 
     for i, (optic, tw) in enumerate(zip(optics, orbits)):
-        fig = plt.figure(1, figsize=(8, 8))
+        fig = create_twiss_figure(model)
         plot_orbit(fig, model, i, tw, measured,
                    base_orbit=base_orbit and base_orbit[i])
+        fig.suptitle(f"Orbit for optic #{i}")
         if save_to is None:
             plt.show()
         else:
             knob = next(iter(optic), (None, None))[0]
-            plt.savefig('{}-orbit-{}-{}.png'.format(save_to, i, knob))
+            plt.savefig(f'{save_to}-orbit-{i}-{knob}.png', dpi=300)
         plt.clf()
 
 
@@ -26,13 +28,15 @@ def make_monitor_plots(
         save_to=None, base_orm=None):
     for index, monitor in enumerate(measured.monitors):
         if monitor in monitor_subset:
+            fig = create_twiss_figure(model)
             plot_monitor_response(
-                plt.figure(1, figsize=(8, 8)), monitor,
+                fig, monitor,
                 model, measured, base_orm, model_orbits, comment)
+            fig.suptitle("Orbit response at {monitor}")
             if save_to is None:
                 plt.show()
             else:
-                plt.savefig('{}-mon-{}-{}.png'.format(save_to, index, monitor))
+                plt.savefig(f'{save_to}-mon-{index}-{monitor}.png', dpi=300)
             plt.clf()
 
 
@@ -41,13 +45,15 @@ def make_steerer_plots(
         save_to=None, base_orm=None):
     for index, steerer in enumerate(measured.steerers):
         if steerer in steerer_subset:
+            fig = create_twiss_figure(model)
             plot_steerer_response(
-                plt.figure(1, figsize=(8, 8)), steerer,
+                fig, steerer,
                 model, measured, base_orm, model_orbits, comment)
+            fig.suptitle("Orbit response due to {steerer}")
             if save_to is None:
                 plt.show()
             else:
-                plt.savefig('{}-ste-{}-{}.png'.format(save_to, index, steerer))
+                plt.savefig(f'{save_to}-ste-{index}-{steerer}.png', dpi=300)
             plt.clf()
 
 
@@ -64,9 +70,8 @@ def plot_monitor_response(
     if stddev is not None:
         stddev = (stddev[:, :, 1:]**2 + stddev[:, :, [0]]**2)**0.5
 
-    for j, ax in enumerate("xy"):
+    for j, ax, axes in zip(range(2), "xy", fig.axes):
 
-        axes = fig.add_subplot(1, 2, 1+j)
         axes.set_title(ax)
         axes.set_xlabel(r"steerer position [m]")
         if ax == 'x':
@@ -110,10 +115,9 @@ def plot_steerer_response(
     if stddev is not None:
         stddev = (stddev[:, :, 1:]**2 + stddev[:, :, [0]]**2)**0.5
 
-    for j, ax in enumerate("xy"):
-        axes = fig.add_subplot(1, 2, 1+j)
+    for j, ax, axes in zip(range(2), "xy", fig.axes):
         axes.set_title(ax)
-        axes.set_xlabel(r"monitor position [m]")
+        axes.set_xlabel(r"Position [m]")
         if ax == 'x':
             axes.set_ylabel(r"orbit response $\Delta x/\Delta \phi$ [mm/mrad]")
         else:
@@ -152,20 +156,36 @@ def plot_orbit(fig, model, i, twiss, measured, base_orbit):
     orbit = measured.orbits[:, :, i]
     error = measured.stddev[:, :, i]
 
-    for j, ax in enumerate("xy"):
-        axes = fig.add_subplot(1, 2, 1+j)
-        axes.set_title(ax)
-        axes.set_xlabel(r"monitor position [m]")
-        if ax == 'x':
-            axes.set_ylabel(r"orbit response $\Delta x/\Delta \phi$ [mm/mrad]")
-        else:
-            axes.yaxis.tick_right()
+    for j, ax, axes in zip(range(2), "xy", fig.axes):
 
-        axes.errorbar(xpos, orbit[:, j], error[:, j],
-                      fmt='v-', label=ax + " measured")
-        axes.plot(twiss.s, twiss[ax], label=ax + " model")
+        axes.set_ylabel(rf"${ax}$ [mm]")
+
+        axes.errorbar(xpos, orbit[:, j] * 1000, error[:, j] * 1000,
+                      fmt='o', label="measured", markersize=5)
+        axes.plot(twiss.s, twiss[ax]*1000, label="model",
+                  **with_outline({}))
         if base_orbit is not None:
-            axes.plot(base_orbit.s, base_orbit[ax], label=ax + " base_orbit")
-        axes.legend()
+            axes.plot(
+                base_orbit.s, base_orbit[ax], label=ax + " base_orbit")
+
+        if ax == 'x':
+            axes.legend(loc='upper right')
 
     fig.suptitle("orbit")
+
+
+def create_twiss_figure(model):
+    fig = plt.figure(1)
+    axx = fig.add_subplot(2, 1, 1)
+    axy = fig.add_subplot(2, 1, 2, sharex=axx)
+    axy.set_xlabel("Position $s$ [m]")
+    for ax, axis in zip((axx, axy), "xy"):
+        ax.x_name = ['s']
+        ax.y_name = [axis]
+        plot_element_indicators(ax, model.elements, effects=background_style)
+        ax.grid(True, axis='y')
+    return fig
+
+
+def background_style(style):
+    return dict(style, alpha=0.2)
